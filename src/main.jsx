6b1f908bed309e676eeecfe563f5aaed599d3791
davaleba-2 <erisiaduke>
2025-12-06 17:17:52 +0400

    ass

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import connection from './App'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <connection/>
+    <App />
   </StrictMode>,
 )
