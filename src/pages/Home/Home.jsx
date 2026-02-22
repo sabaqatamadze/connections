fe8b0164cecd0557326d451d1a94506e6a449463
davaleba-2 <erisiaduke>
2026-02-22 20:28:58 +0400

    fourth layer part 1 finished

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Home.css";
+import ReactPlayer from 'react-player'
 import { Link } from "react-router-dom";
+import { LuCrown } from "react-icons/lu";
+import { FaCompassDrafting } from "react-icons/fa6";
+import { TbPlaystationTriangle } from "react-icons/tb";
 
 
 function Home() {
@@ -83,6 +87,63 @@ function Home() {
 
 
     </div>
+
+    <div className="third-layer">
+     
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=KAQUhK0-GJc"
+        controls={true}     
+        width="100%"
+        height="100%"
+      />
+
+    </div>
+
+
+    <div className="fourth-layer">
+   <img
+    src="https://res.cloudinary.com/djont7dcv/image/upload/v1769357451/picture1_ctqtuc.jpg"
+    alt="background"
+    className="fourth-img"
+  />
+
+  <div className="fourth-content">
+    <h1 className="title">Why Us?</h1>
+    <div className="features">
+      <div className="box">
+        <p className="icon" style={{ transform: "rotate(180deg)", fontSize: "28px", marginTop: "20px" }}>
+        <TbPlaystationTriangle />
+        </p> 
+        <h1 className="servise">GUARANTEE</h1>
+        <p className="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        
+      </div>
+      <div className="box">
+        <p className="icon" >
+        <LuCrown />
+        </p> 
+        <h1 className="servise">SERVISE</h1>
+        <p className="text">
+          Nunc vulputate libero et velit interdum, ac aliquet odio.
+        </p>
+        
+        
+        </div>
+      <div className="box">
+        <p className="icon">
+        <FaCompassDrafting />
+        </p> 
+        <h1 className="servise">EXPERIENCE</h1>
+        <p className="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        
+      </div>
+    </div>
+  </div>
+</div>
       
 
       
