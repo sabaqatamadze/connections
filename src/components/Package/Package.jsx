a8a3e7cacd6c952d2ef4247a056eb8f0b7f4539c
davaleba-2 <erisiaduke>
2026-04-05 22:07:48 +0600

    started package

--- src/components/Package/Package.jsx
+++ src/components/Package/Package.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import "./Package.css";
 
 function Package() {
   return (
-    <div>package</div>
+    <div className="pricing-container">
+        <div className="pricing-cards-wrapper">
+            <div className="pricing-card theme-blue">
+                <div className="card-icon">
+                    {/* Your Icon Here */}
+                </div>
+    
+                <h2 className="card-title">{title}</h2>
+    
+                <ul className="card-features">
+                    
+                  {features.map((item, index) => (
+              <li key={index}>
+                {item.name}
+              </li>
+            ))}
+                </ul>
+    
+                <button className="card-button">{footerText}</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
