b08366edb43354dc1678964daf01383e878066ba
davaleba-2 <erisiaduke>
2026-04-09 19:06:07 +0400

    fix api

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,22 +1,12 @@
 import axios from "../config/axios";
 
-const API_KEY = "ef09e8d442a03f39553f19a71ad2676e";
-
 async function getAllPackage() {
-  const resp = await axios.get("packages", {
-    headers: {
-      "x-api-key": API_KEY,
-    },
-  });
+  const resp = await axios.get("packages");
   return resp.data;
 }
 
 async function getSinglePackage(product_id) {
-  const resp = await axios.get(`packages/${product_id}`, {
-    headers: {
-      "x-api-key": API_KEY,
-    },
-  });
+  const resp = await axios.get(`packages/${product_id}`);
   return resp.data;
 }
 
