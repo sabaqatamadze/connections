dbd8579073ad180515a5da00308fed4ceee6a7ac
davaleba-2 <erisiaduke>
2026-04-09 18:38:28 +0400

    responsive is done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,8 +5,18 @@ import { FaPhone } from "react-icons/fa6";
 import { MdMailOutline } from "react-icons/md";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 function NavBar() {
+
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+
+
   return (
     <div className="Nav-bar">
       <div className="Links">
@@ -33,13 +43,18 @@ function NavBar() {
         </div>
       </div>
 
-      <div className="Pages">
+      <div className={isMenuOpen ? 'Pages active' : 'Pages'} >
         <Link to="/">Home</Link>
         <Link to="/Packages">Packages</Link>
         <Link to="/about">About us</Link>
         <Link to="/contact">Contact</Link>
       </div>
+
+      <div className="res-icon" onClick={toggleMenu}>
+        <FaBars />
+        </div>
     </div>
+    
   );
 }
 
--- src/components/Package/Package.jsx
+++ src/components/Package/Package.jsx
@@ -4,26 +4,26 @@ import "./Package.css";
 function Package() {
   return (
     <div className="pricing-container">
-        <div className="pricing-cards-wrapper">
-            <div className="pricing-card theme-blue">
-                <div className="card-icon">
-                    {/* Your Icon Here */}
-                </div>
-    
-                <h2 className="card-title">{title}</h2>
-    
-                <ul className="card-features">
-                    
-                  {features.map((item, index) => (
+      <div className="pricing-cards-wrapper">
+        <div className="pricing-card theme-blue">
+          <div className="card-icon">
+            {/* Your Icon Here */}
+          </div>
+
+          <h2 className="card-title">{title}</h2>
+
+          <ul className="card-features">
+
+            {features.map((item, index) => (
               <li key={index}>
                 {item.name}
               </li>
             ))}
-                </ul>
-    
-                <button className="card-button">{footerText}</button>
-            </div>
+          </ul>
+
+          <button className="card-button">{footerText}</button>
         </div>
+      </div>
     </div>
   )
 }
