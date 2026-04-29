a4166c0f54be552d6d38dddd38cbc5ef74e912a5
davaleba-2 <erisiaduke>
2026-04-29 22:31:10 +0400

    footer

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../components/footer/Footer";
+import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 
 
