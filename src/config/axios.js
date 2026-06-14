494ce29e063b23e7376cc153369f4890a875cd60
davaleba-2 <sabagamer098@gmail.com>
2026-06-14 18:17:13 +0400

    fix

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const config = axios.create({
   baseURL: "https://connections-backend-k0s7.onrender.com",
   headers: {
-    "x-api-key": "ef09e8d442a03f39553f19a71ad2676e",
+    "x-api-key": "import.meta.env.VITE_API_KEY",
   },
 });
 export default config;
