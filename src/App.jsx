05defd360d1d7998303eb873f3d99efd9b246029
davaleba-2 <sabagamer098@gmail.com>
2026-07-20 10:16:19 +0400

    speed insight

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Myroutes from "./myroutes";
 import "./App.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <div>
       <Myroutes />
       <Analytics />
+      <SpeedInsights />
+      
     
     </div>
   );
