73b60abf8fa5b6f75bfb7db0b660dbcfdbf55d20
davaleba-2 <sabagamer098@gmail.com>
2026-07-20 10:09:11 +0400

    versel analistic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Myroutes from "./myroutes";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div>
       <Myroutes />
+      <Analytics />
+    
     </div>
   );
 }
