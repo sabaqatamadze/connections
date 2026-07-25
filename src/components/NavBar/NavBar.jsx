dcc14a92627437d50ed8f10e5f03dd4ebbfb7e47
davaleba-2 <sabagamer098@gmail.com>
2026-07-26 00:02:45 +0400

    nav bar fix

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,60 +1,133 @@
 import React from "react";
-import { FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import {
+  FaInstagram,
+  FaBars,
+  FaTimes,
+  FaPhone,
+} from "react-icons/fa";
 import { SlSocialFacebook } from "react-icons/sl";
-import { FaPhone } from "react-icons/fa6";
 import { MdMailOutline } from "react-icons/md";
 import "./NavBar.css";
-import { Link } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
 
 function NavBar() {
-
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-
-
   return (
     <div className="Nav-bar">
       <div className="Links">
-        <div className="social"> 
+        <div className="social">
           <a
-          href="https://www.instagram.com/the_connectionss?igsh=d3dvdGx4emtoZWoy&utm_source=qr"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaInstagram  />
-        </a>
-        <a
-          href="https://www.facebook.com/profile.php?id=61592185824116&rdid=ETIaj136RAabpmRb&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1Kxrk8oBVf%2F#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <SlSocialFacebook />
-        </a>
-        </div>
-        <div className="contact">
-          <p><FaPhone  className="Icon"/>+995 551 51 83 51</p>
-          <p><MdMailOutline className="Icon"/>rightconnections90@gmail.com</p>
+            href="https://www.instagram.com/the_connectionss?igsh=d3dvdGx4emtoZWoy&utm_source=qr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
+          </a>
 
+          <a
+            href="https://www.facebook.com/profile.php?id=61592185824116"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SlSocialFacebook />
+          </a>
+        </div>
+
+        <div className="contact">
+          <p>
+            <FaPhone className="Icon" />
+            +995 551 51 83 51
+          </p>
+
+          <p>
+            <MdMailOutline className="Icon" />
+            rightconnections90@gmail.com
+          </p>
+        </div>
+
+        <div className="res-icon" onClick={toggleMenu}>
+          {isMenuOpen ? <FaTimes /> : <FaBars />}
         </div>
       </div>
 
-      <div className={isMenuOpen ? 'Pages active' : 'Pages'} >
+      <div
+        className={isMenuOpen ? "overlay active" : "overlay"}
+        onClick={toggleMenu}
+      ></div>
+
+      <div className={isMenuOpen ? "Pages active" : "Pages"}>
+        <div className="menu-header">
+          <h2>Connections</h2>
+          <span>Travel Georgia</span>
+        </div>
+
+        <div className="menu-links">
+          <Link to="/" onClick={toggleMenu}>
+            Home
+          </Link>
+
+          <Link to="/Packages" onClick={toggleMenu}>
+            Packages
+          </Link>
+
+          <Link to="/about" onClick={toggleMenu}>
+            About Us
+          </Link>
+
+          <Link to="/contact" onClick={toggleMenu}>
+            Contact
+          </Link>
+        </div>
+
+        <div className="menu-bottom">
+          <div className="menu-contact">
+            <p>
+              <FaPhone />
+              +995 551 51 83 51
+            </p>
+
+            <p>
+              <MdMailOutline />
+              rightconnections90@gmail.com
+            </p>
+          </div>
+
+          <div className="menu-social">
+            <a
+              href="https://www.instagram.com/the_connectionss?igsh=d3dvdGx4emtoZWoy&utm_source=qr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+
+            <a
+              href="https://www.facebook.com/profile.php?id=61592185824116"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <SlSocialFacebook />
+            </a>
+          </div>
+
+          <div className="menu-footer">
+            Plan your next adventure 🌴
+          </div>
+        </div>
+      </div>
+
+      <div className="desktop-pages">
         <Link to="/">Home</Link>
         <Link to="/Packages">Packages</Link>
-        <Link to="/about">About us</Link>
+        <Link to="/about">About Us</Link>
         <Link to="/contact">Contact</Link>
       </div>
-
-      <div className="res-icon" onClick={toggleMenu}>
-        <FaBars />
-        </div>
     </div>
-    
   );
 }
 
